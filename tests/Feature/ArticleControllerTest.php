b2fe77ff185dde3aac1db808544852829a64a5e5
YUYU-07 <mammam0501@yahoo.co.jp>
2020-09-26 05:01:11 +0900

    yml変更

---
+++ tests/Feature/ArticleControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    //一覧表示テスト
+    public function testIndex()
+    {
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.index');
+    }
+    
+    //未ログイン時テスト
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    //ログイン時投稿テスト
+    public function testAuthCreate()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('articles.create'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.create');
+    }
+}
