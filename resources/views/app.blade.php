2aa811cd29c4377b6d53838244573eef48c6e20e
YUYU-07 <mammam0501@yahoo.co.jp>
2020-10-04 06:09:43 +0900

    README編集

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
   <title>
     @yield('title')
   </title>
+  <link rel="shortcut icon" href="images/favicon.ico">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
   <!-- Bootstrap core CSS -->
